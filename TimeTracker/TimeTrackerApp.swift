3fbe669519923cf2bfab5d2be187029d0a0fc804
Тришин Сергей <sergey.trishin@gazprombank.ru>
2026-01-06 07:45:55 +1000

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Сергей Тришин on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
